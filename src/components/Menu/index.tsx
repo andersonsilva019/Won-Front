4460584ab33949113fe2f0998d0ae6993a0238e8
Anderson Silva <andersonnsilva015@gmail.com>
2021-01-03 20:31:41 -0300

    :sparkles: feat: create component Menu

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+import { RiMenu2Line as MenuIcon } from 'react-icons/ri'
+import {
+  ShoppingCart as ShoppingCarIcon,
+  Search as SeacrIcon,
+  Close as CloseIcon
+} from '@styled-icons/material-outlined'
+import Logo from 'components/Logo'
+import * as S from './styles'
+import Button from '../Button'
+
+const Menu = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <S.Container>
+      <S.IconWrapper onClick={() => setIsOpen(true)}>
+        <MenuIcon size={20} aria-label="Open Menu" />
+      </S.IconWrapper>
+      <S.LogoWrapper>
+        <Logo hideOnMobile />
+      </S.LogoWrapper>
+      <S.MenuGroup>
+        <S.IconWrapper>
+          <SeacrIcon aria-label="Search" />
+        </S.IconWrapper>
+        <S.IconWrapper>
+          <ShoppingCarIcon aria-label="Open Shopping Cart" />
+        </S.IconWrapper>
+      </S.MenuGroup>
+      <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
+        <CloseIcon aria-label="Close Menu" onClick={() => setIsOpen(false)} />
+        <S.MenuNav>
+          <S.MenuLink href="#">Home</S.MenuLink>
+          <S.MenuLink href="#">Explore</S.MenuLink>
+        </S.MenuNav>
+
+        <S.RegisterBox>
+          <Button fullWidth size="large">
+            Log in now
+          </Button>
+          <span>or</span>
+          <S.CreateAccount href="#" title="Sign Up">
+            Sign Up
+          </S.CreateAccount>
+        </S.RegisterBox>
+      </S.MenuFull>
+    </S.Container>
+  )
+}
+
+export default Menu

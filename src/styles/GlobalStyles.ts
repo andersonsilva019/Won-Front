24736524042002c1ae3c9885e9e67208dc145c87
Anderson Silva <andersonnsilva015@gmail.com>
2021-01-05 15:38:58 -0300

    :sparkles: feat: Add footer

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -1,39 +1,35 @@
 import { createGlobalStyle, css } from 'styled-components'
 
 export default createGlobalStyle`
-  /* poppins-300 - latin */
-  @font-face {
+   @font-face {
     font-family: 'Poppins';
     font-style: normal;
     font-weight: 300;
     font-display: swap;
-    src: local(''),
-        url('/fonts/poppins-v15-latin-300.woff2') format('woff2'),
+    src: local('Poppins Light'), local('Poppins-Light'),
+        url('/fonts/poppins-v15-latin-300.woff2') format('woff2');
   }
-  /* poppins-regular - latin */
   @font-face {
     font-family: 'Poppins';
     font-style: normal;
     font-weight: 400;
     font-display: swap;
-    src: local(''),
-        url('/fonts/poppins-v15-latin-regular.woff2') format('woff2'),
+    src: local('Poppins Regular'), local('Poppins-Regular'),
+        url('/fonts/poppins-v15-latin-regular.woff2') format('woff2');
   }
-  /* poppins-600 - latin */
   @font-face {
     font-family: 'Poppins';
     font-style: normal;
     font-weight: 600;
     font-display: swap;
-    src: local(''),
-        url('/fonts/poppins-v15-latin-600.woff2') format('woff2'),
+    src: local('Poppins SemiBold'), local('Poppins-SemiBold'),
+        url('/fonts/poppins-v15-latin-600.woff2') format('woff2');
   }
 
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    outline: 0;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
@@ -42,10 +38,9 @@ export default createGlobalStyle`
     html {
       font-size: 62.5%;
     }
-
     body {
-      font-family: ${theme.font.family}
-      font-size: ${theme.font.sizes.medium}
+      font-family: ${theme.font.family};
+      font-size: ${theme.font.sizes.medium};
     }
   `}
 `

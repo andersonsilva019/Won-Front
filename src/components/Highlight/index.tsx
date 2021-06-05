098ac2c05bad04a45de9cf90cfffb1bb99282507
Anderson Silva <andersonnsilva@alu.ufc.br>
2021-06-05 20:41:40 -0300

    :sparkles: feat: creating Highlight component and running first test

---
+++ src/components/Highlight/index.tsx
@@ -0,0 +1,28 @@
+import Button from '../Button'
+import * as S from './styles'
+
+export type HighlightProps = {
+  title: string
+  subtitle: string
+  buttonLabel: string
+  buttonLink: string
+}
+
+const Highlight = ({
+  title,
+  subtitle,
+  buttonLabel,
+  buttonLink
+}: HighlightProps) => {
+  return (
+    <S.Container>
+      <S.Title>{title}</S.Title>
+      <S.Subtitle>{subtitle}</S.Subtitle>
+      <Button as="a" href={buttonLink}>
+        {buttonLabel}
+      </Button>
+    </S.Container>
+  )
+}
+
+export default Highlight

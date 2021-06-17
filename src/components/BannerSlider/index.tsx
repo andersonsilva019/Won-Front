bce5b7f9dcc001bb0aa220636517d97bd0879e45
Anderson Silva <andersonnsilva@alu.ufc.br>
2021-06-17 20:08:26 -0300

    :sparkles: feat: creating BannerSlider components

---
+++ src/components/BannerSlider/index.tsx
@@ -0,0 +1,38 @@
+import Banner, { BannerProps } from '../Banner'
+import Slider, { SliderSettings } from '../Slider'
+import * as S from './styles'
+
+export type BannerSliderProps = {
+  items: BannerProps[]
+}
+
+const settings: SliderSettings = {
+  dots: true,
+  arrows: false,
+  vertical: true,
+  verticalSwiping: true,
+  infinite: false,
+  responsive: [
+    {
+      breakpoint: 1170,
+      settings: {
+        vertical: false,
+        verticalSwiping: false
+      }
+    }
+  ]
+}
+
+const BannerSlider = ({ items }: BannerSliderProps) => {
+  return (
+    <S.Container>
+      <Slider settings={settings}>
+        {items.map(item => (
+          <Banner {...item} key={item.title} />
+        ))}
+      </Slider>
+    </S.Container>
+  )
+}
+
+export default BannerSlider

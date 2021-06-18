829d497d9ea0e33cb26ededf55190b1143a915df
Anderson Silva <andersonnsilva@alu.ufc.br>
2021-06-18 09:47:02 -0300

    :sparkles: feat: creating Container component

---
+++ src/components/Container/test.tsx
@@ -0,0 +1,35 @@
+import { renderWithTheme } from 'utils/test/helpers'
+import theme from 'styles/theme'
+import { Container } from '.'
+
+describe('<Container />', () => {
+  it('should render correctly', () => {
+    const { container } = renderWithTheme(
+      <Container>
+        <span>My Website</span>
+      </Container>
+    )
+
+    expect(container.firstChild).toHaveStyle({
+      maxWidth: theme.grid.container
+    })
+
+    expect(container.firstChild).toMatchInlineSnapshot(`
+      .c0 {
+        max-width: 130rem;
+        margin-left: auto;
+        margin-right: auto;
+        padding-left: calc(3.2rem / 2);
+        padding-right: calc(3.2rem / 2);
+      }
+
+      <div
+        class="c0"
+      >
+        <span>
+          My Website
+        </span>
+      </div>
+    `)
+  })
+})

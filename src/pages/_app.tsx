318887d374f72a7ebbd6d7b7e2364c3050a6e95b
Anderson Silva <andersonnsilva@alu.ufc.br>
2021-06-18 11:15:39 -0300

    :sparkles: feat: creating Home page with Menu and Footer sections

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,13 +8,13 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <Head>
-        <title>React Avançado - Boilerplate</title>
+        <title>Won Games</title>
         <link rel="shortcut icon" href="/images/icon-512.png" />
         <link rel="apple-touch-icon" href="/images/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+          content="The best games stores in the world!"
         />
       </Head>
 

f4f7b0f26c296f4ae3eb86dac947497d6ed64abc
Anderson Silva <andersonnsilva@alu.ufc.br>
2021-06-23 09:23:26 -0300

    :sparkles: feat: adding mocks - Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,26 @@
 import { GetStaticProps } from 'next'
 import Home, { HomeTemplateProps } from 'templates/Home'
 
+import bannersMock from 'components/BannerSlider/mock'
+import gamesMock from 'components/GameCardSlider/mock'
+import highLightMock from 'components/Highlight/mock'
+
 export default function Index(props: HomeTemplateProps) {
   return <Home {...props} />
 }
 
-export const getStaticProps: GetStaticProps = async () => {
+export const getStaticProps: GetStaticProps<HomeTemplateProps> = async () => {
   return {
-    props: {}
+    props: {
+      banners: bannersMock,
+      freeGames: gamesMock,
+      freeHighligth: highLightMock,
+      mostPopularGames: gamesMock,
+      mostPopularHighlight: highLightMock,
+      newGames: gamesMock,
+      upcommingGames: gamesMock,
+      upcommingHighligth: highLightMock,
+      upcommingMoreGames: gamesMock
+    }
   }
 }

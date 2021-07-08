ddaa547114fa4331e32fdb464e293adb428ed4f4
Anderson Silva <andersonnsilva@alu.ufc.br>
2021-07-07 22:03:13 -0300

    :sparkles: feat: creating FormSignUp component

---
+++ src/components/FormSignUp/index.tsx
@@ -0,0 +1,49 @@
+import Link from 'next/link'
+import { Email, Lock, AccountCircle } from '@styled-icons/material-outlined'
+import Button from '../Button'
+import TextField from '../TextField'
+import * as S from './styles'
+
+const FormSignUp = () => {
+  return (
+    <S.Container>
+      <form>
+        <TextField
+          type="text"
+          name="name"
+          placeholder="Nome"
+          icon={<AccountCircle />}
+        />
+        <TextField
+          name="email"
+          placeholder="Email"
+          type="email"
+          icon={<Email />}
+        />
+        <TextField
+          name="password"
+          placeholder="Password"
+          type="password"
+          icon={<Lock />}
+        />
+        <TextField
+          name="confirm_password"
+          placeholder="Confirm password"
+          type="password"
+          icon={<Lock />}
+        />
+        <Button size="large" fullWidth>
+          Sign up now
+        </Button>
+        <S.FormLink>
+          Already have an account?
+          <Link href="/sign-in">
+            <a>Sign in</a>
+          </Link>
+        </S.FormLink>
+      </form>
+    </S.Container>
+  )
+}
+
+export default FormSignUp

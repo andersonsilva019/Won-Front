84c722dc8d84e6a127fc32ee41da8d136eb6b6f8
Anderson Silva <andersonnsilva@alu.ufc.br>
2021-08-30 13:42:20 -0300

    feat: creating initial version of GameItem

---
+++ src/components/GameItem/index.tsx
@@ -0,0 +1,25 @@
+import * as S from './styles'
+
+export type GameItemProps = {
+  img: string
+  title: string
+  price: string
+}
+
+const GameItem = ({ price, img, title }: GameItemProps) => {
+  return (
+    <S.Container>
+      <S.GameContent>
+        <S.ImageBox>
+          <img src={img} alt={title} />
+        </S.ImageBox>
+        <S.Content>
+          <S.Title>{title}</S.Title>
+          <S.Price>{price}</S.Price>
+        </S.Content>
+      </S.GameContent>
+    </S.Container>
+  )
+}
+
+export default GameItem
---
+++ src/components/GameItem/test.tsx
@@ -0,0 +1,26 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/test/helpers'
+import GameItem, { GameItemProps } from '.'
+
+const props: GameItemProps = {
+  img: 'https://source.unsplash.com/user/willianjusten/151x70',
+  title: 'Red Dead Redemption 2',
+  price: 'R$ 215,00'
+}
+
+describe('<GameItem />', () => {
+  it('should render correcly', () => {
+    renderWithTheme(<GameItem {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: props.title })
+    ).toBeInTheDocument()
+
+    expect(screen.getByText(props.price)).toBeInTheDocument()
+
+    expect(screen.getByRole('img', { name: props.title })).toHaveAttribute(
+      'src',
+      props.img
+    )
+  })
+})

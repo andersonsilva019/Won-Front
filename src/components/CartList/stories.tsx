c9e7094a1803cf2f681465100520526360f73024
Anderson Silva <andersonnsilva@alu.ufc.br>
2021-08-30 16:12:06 -0300

    feat: creating CartList component (#10)

---
+++ src/components/CartList/stories.tsx
@@ -0,0 +1,29 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import CartList, { CartListProps } from '.'
+
+import mockGameItem from './mock'
+
+export default {
+  title: 'CartList',
+  component: CartList,
+  args: {
+    items: mockGameItem,
+    total: 'R$ 310,00'
+  },
+  argTypes: {
+    items: {
+      type: ''
+    }
+  },
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    }
+  }
+} as Meta
+
+export const Basic: Story<CartListProps> = args => (
+  <div style={{ maxWidth: '80rem' }}>
+    <CartList {...args} />
+  </div>
+)

8aa71dbed0cb8461de36fe90360d22b5edfaebe2
Anderson Silva <andersonnsilva@alu.ufc.br>
2021-09-20 14:11:14 -0300

    feat: get banner data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,37 @@
 import { GetStaticProps } from 'next'
 import Home, { HomeTemplateProps } from 'templates/Home'
 
-import bannersMock from 'components/BannerSlider/mock'
 import gamesMock from 'components/GameCardSlider/mock'
 import highLightMock from 'components/Highlight/mock'
+import { initializeApollo } from '../../lib/apolloClient'
+import { QueryHome } from 'graphql/generated/QueryHome'
+import { QUERY_HOME } from 'graphql/queries/home'
 export default function Index(props: HomeTemplateProps) {
   return <Home {...props} />
 }
 
-export const getStaticProps: GetStaticProps<HomeTemplateProps> = async () => {
+export const getStaticProps: GetStaticProps = async () => {
+  const apolloClient = initializeApollo()
+
+  const { data } = await apolloClient.query<QueryHome>({ query: QUERY_HOME })
+
+  const banners = data.banners.map(banner => ({
+    img: `http://localhost:1337${banner.image?.url}`,
+    title: banner.title,
+    subtitle: banner.subtitle,
+    buttonLabel: banner.button?.label,
+    buttonLink: banner.button?.link,
+    ...(banner.ribbon && {
+      ribbon: banner.ribbon?.text,
+      ribbonColor: banner.ribbon?.color,
+      ribbonSize: banner.ribbon?.size
+    })
+  }))
+
   return {
+    revalidate: 60, // In seconds
     props: {
-      banners: bannersMock,
+      banners,
       freeGames: gamesMock,
       freeHighligth: highLightMock,
       mostPopularGames: gamesMock,

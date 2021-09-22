4a77fa41e284beb831743580cd601119a6be8e49
Anderson Silva <andersonnsilva@alu.ufc.br>
2021-09-22 09:20:46 -0300

    refactor: using mappers

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
 import { GetStaticProps } from 'next'
 import Home, { HomeTemplateProps } from 'templates/Home'
 
-import gamesMock from 'components/GameCardSlider/mock'
-import highLightMock from 'components/Highlight/mock'
 import { initializeApollo } from '../../lib/apolloClient'
 import { QueryHome } from 'graphql/generated/QueryHome'
 import { QUERY_HOME } from 'graphql/queries/home'
+import { bannerMapper, gamesMapper, highlightMapper } from 'utils/mappers'
+
 export default function Index(props: HomeTemplateProps) {
   return <Home {...props} />
 }
@@ -20,86 +20,22 @@ export const getStaticProps: GetStaticProps = async () => {
   return {
     revalidate: 60, // In seconds
     props: {
-      banners: banners.map(banner => ({
-        img: `http://localhost:1337${banner.image?.url}`,
-        title: banner.title,
-        subtitle: banner.subtitle,
-        buttonLabel: banner.button?.label,
-        buttonLink: banner.button?.link,
-        ...(banner.ribbon && {
-          ribbon: banner.ribbon?.text,
-          ribbonColor: banner.ribbon?.color,
-          ribbonSize: banner.ribbon?.size
-        })
-      })),
+      banners: bannerMapper(banners),
+
       newGamesTitle: sections?.newGames?.title,
-      newGames: newGames.map(game => ({
-        slug: game.slug,
-        title: game.name,
-        developer: game.developers[0].name,
-        img: `http://localhost:1337${game.cover?.url}`,
-        price: game.price
-      })),
+      newGames: gamesMapper(newGames),
+
       freeGamesTitle: sections?.freeGames?.title,
-      freeHighligth: {
-        title: sections?.freeGames?.highlight?.title,
-        subtitle: sections?.freeGames?.highlight?.subtitle,
-        backgroundImage: `http://localhost:1337${sections?.freeGames?.highlight?.background?.url}`,
-        floatImage: `http://localhost:1337${sections?.freeGames?.highlight?.floatImage?.url}`,
-        buttonLink: sections?.freeGames?.highlight?.buttonLink,
-        buttonLabel: sections?.freeGames?.highlight?.buttonLabel,
-        alignment: sections?.freeGames?.highlight?.alignment
-      },
-      freeGames: freeGames.map(game => ({
-        slug: game.slug,
-        title: game.name,
-        developer: game.developers[0].name,
-        img: `http://localhost:1337${game.cover?.url}`,
-        price: game.price
-      })),
+      freeHighligth: highlightMapper(sections?.freeGames?.highlight),
+      freeGames: gamesMapper(freeGames),
+
       mostPopularGamesTitle: sections?.popularGames?.title,
-      mostPopularHighlight: {
-        title: sections?.popularGames?.highlight?.title,
-        subtitle: sections?.popularGames?.highlight?.subtitle,
-        backgroundImage: `http://localhost:1337${sections?.popularGames?.highlight?.background?.url}`,
-        floatImage: `http://localhost:1337${sections?.popularGames?.highlight?.floatImage?.url}`,
-        buttonLink: sections?.popularGames?.highlight?.buttonLink,
-        buttonLabel: sections?.popularGames?.highlight?.buttonLabel,
-        alignment: sections?.popularGames?.highlight?.alignment
-      },
-      mostPopularGames: sections!.popularGames!.games.map(game => ({
-        slug: game.slug,
-        title: game.name,
-        developer: game.developers[0].name,
-        img: `http://localhost:1337${game.cover?.url}`,
-        price: game.price
-      })),
+      mostPopularHighlight: highlightMapper(sections?.popularGames?.highlight),
+      mostPopularGames: gamesMapper(sections!.popularGames!.games),
+
       upcommingGamesTitle: sections?.upcomingGames?.title,
-      upcommingHighligth: {
-        title: sections?.upcomingGames?.highlight?.title,
-        subtitle: sections?.upcomingGames?.highlight?.subtitle,
-        backgroundImage: `http://localhost:1337${sections?.upcomingGames?.highlight?.background?.url}`,
-        floatImage: `http://localhost:1337${sections?.upcomingGames?.highlight?.floatImage?.url}`,
-        buttonLink: sections?.upcomingGames?.highlight?.buttonLink,
-        buttonLabel: sections?.upcomingGames?.highlight?.buttonLabel,
-        alignment: sections?.upcomingGames?.highlight?.alignment
-      },
-      upcommingGames: upcomingGames.map(game => ({
-        slug: game.slug,
-        title: game.name,
-        developer: game.developers[0].name,
-        img: `http://localhost:1337${game.cover?.url}`,
-        price: game.price
-      }))
+      upcommingHighligth: highlightMapper(sections?.upcomingGames?.highlight),
+      upcommingGames: gamesMapper(upcomingGames)
     }
   }
 }
-
-// {
-//   slug: 'population-zero',
-//   title: 'Population Zero',
-//   developer: 'Rockstar Games',
-//   img: 'https://source.unsplash.com/user/willianjusten/300x140',
-//   price: 235.0,
-//   promotionalPrice: 215.0
-// },

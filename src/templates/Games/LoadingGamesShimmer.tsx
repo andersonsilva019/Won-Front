0bd13a8eda1cfe8ed1178b0dc4e1be733083b9fd
Anderson Silva <andersonnsilva@alu.ufc.br>
2021-09-26 17:51:05 -0300

    feat: creating state loading

---
+++ src/templates/Games/LoadingGamesShimmer.tsx
@@ -0,0 +1,57 @@
+import styled, { css } from 'styled-components'
+import Skeletron from 'components/Skeletron'
+
+export function LoadingGamesShimmer() {
+  return (
+    <div>
+      <Container>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9].map(item => (
+          <ShimmerGame key={item} />
+        ))}
+      </Container>
+    </div>
+  )
+}
+
+function ShimmerGame() {
+  return (
+    <ShimmerContainer>
+      <Skeletron className="image-skeletron" />
+      <Skeletron className="title-skeletron" />
+      <Skeletron className="subtitle-skeletron" />
+    </ShimmerContainer>
+  )
+}
+
+const Container = styled.div`
+  ${({ theme }) => css`
+    display: grid;
+    margin: ${theme.spacings.medium} 0;
+    grid-template-columns: repeat(auto-fill, minmax(25rem, 1fr));
+    grid-gap: ${theme.spacings.medium};
+  `}
+`
+
+const ShimmerContainer = styled.section`
+  ${({ theme }) => css`
+    background: ${theme.colors.white};
+    .image-skeletron {
+      height: 14rem;
+      width: 100%;
+    }
+
+    .title-skeletron {
+      margin: 1.6rem 1rem;
+
+      height: 1.6rem;
+      width: 75%;
+    }
+
+    .subtitle-skeletron {
+      height: 1.6rem;
+      width: 50%;
+
+      margin: 0 1rem 2.4rem;
+    }
+  `}
+`
--- src/templates/Games/index.tsx
+++ src/templates/Games/index.tsx
@@ -6,9 +6,11 @@ import { Container } from 'components/Container'
 import ExploreSidebar, { ItemProps } from 'components/ExploreSidebar'
 import { Grid } from 'components/Grid'
 import { KeyboardArrowDown as ArrowDown } from '@styled-icons/material-outlined/KeyboardArrowDown'
-import * as S from './styles'
 import { QueryGames, QueryGamesVariables } from 'graphql/generated/QueryGames'
 import { QUERY_GAMES } from 'graphql/queries/games'
+import * as S from './styles'
+
+import { LoadingGamesShimmer } from './LoadingGamesShimmer'
 
 export type GamesTemplateProps = {
   games?: GameCardProps[]
@@ -33,24 +35,29 @@ const Games = ({ games = [], filterItems = [] }: GamesTemplateProps) => {
       <Container>
         <S.Main>
           <ExploreSidebar items={filterItems} onFilter={handleFilter} />
-          <section>
-            <Grid>
-              {data?.games.map(game => (
-                <GameCard
-                  key={game.slug}
-                  title={game.name}
-                  slug={game.slug}
-                  developer={game.developers[0].name}
-                  img={`http://localhost:1337${game.cover?.url}`}
-                  price={game.price}
-                />
-              ))}
-            </Grid>
-            <S.ShowMore role="button" onClick={handleShowMore}>
-              <p>Show More</p>
-              <ArrowDown size={35} />
-            </S.ShowMore>
-          </section>
+          {/* eslint-disable-next-line */}
+          {loading ? (
+            <LoadingGamesShimmer />
+          ) : (
+            <section>
+              <Grid>
+                {data?.games.map(game => (
+                  <GameCard
+                    key={game.slug}
+                    title={game.name}
+                    slug={game.slug}
+                    developer={game.developers[0].name}
+                    img={`http://localhost:1337${game.cover?.url}`}
+                    price={game.price}
+                  />
+                ))}
+              </Grid>
+              <S.ShowMore role="button" onClick={handleShowMore}>
+                <p>Show More</p>
+                <ArrowDown size={35} />
+              </S.ShowMore>
+            </section>
+          )}
         </S.Main>
       </Container>
     </Base>

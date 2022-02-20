9a05c9e9e4673c3d16205717f65b51de0fdf4b1c
Anderson Silva <andersonnsilva@alu.ufc.br>
2022-02-20 10:17:55 -0300

    feat: Criando parses para os filtros

---
+++ src/utils/filter/index.ts
@@ -0,0 +1,45 @@
+import { ItemProps } from "components/ExploreSidebar"
+import { ParsedUrlQueryInput } from "querystring"
+
+type ParseArgs = {
+  queryString: ParsedUrlQueryInput
+  filterItems: Pick<ItemProps, 'name' | 'type'>[]
+}
+
+export const parseQueryStringToWhere = ({
+  queryString,
+  filterItems
+}: ParseArgs) => {
+  const obj: any = {}
+
+  Object.keys(queryString)
+    .filter(item => item !== 'sort')
+    .forEach(key => {
+      const item = filterItems?.find(item => item.name === key);
+      const isCheckbox = item?.type === 'checkbox'
+
+      obj[key] = !isCheckbox ? queryString[key] : { name_contains: queryString[key] }
+  })
+
+  return obj;
+}
+
+export const parseQueryStringToFilter = ({
+  queryString,
+  filterItems
+}: ParseArgs) => {
+  const obj: any = {}
+
+
+  Object.keys(queryString).forEach(key => {
+    const item = filterItems?.find(item => item.name === key);
+    const isCheckbox = item?.type === 'checkbox'
+    const isArray = Array.isArray(queryString[key])
+    const type = item?.type
+
+      obj[key] = !isArray && isCheckbox ? [queryString[key]] : queryString[key]
+  })
+
+  return obj;
+
+}

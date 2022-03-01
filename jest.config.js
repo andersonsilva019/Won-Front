4ec4a23fa1c8c3fa912de4d6da4a40c0811021c2
Anderson Silva <andersonnsilva@alu.ufc.br>
2022-03-01 20:25:53 -0300

    config: Removendo certos arquivos do coverage

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,10 @@ module.exports = {
     '!src/pages/**/*.tsx',
     '!src/styles/**/*.ts',
     '!src/graphql/**/*.ts',
-    '!src/**/mock.ts'
+    '!src/**/mock.ts',
+    '!src/graphql/**/*.ts',
+    '!src/types/**/*.d.ts',
+    '!src/utils/apolloCache.ts',
   ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/', '<rootDit>/.jest/']

c8e92feadfd3740ed032fd5b008f99bc6e6b36cd
Anderson Silva <andersonnsilva@alu.ufc.br>
2022-03-02 10:43:56 -0300

    feat: Buscando dados atuais dos items via Apollo

--- src/hooks/use-cart/index.tsx
+++ src/hooks/use-cart/index.tsx
@@ -1,10 +1,19 @@
+import { useQueryGames } from 'graphql/queries/games'
 import { createContext, useContext, useEffect, useState } from 'react'
+import { formatPrice } from 'utils/formatPrice'
 import { getStorageItem } from 'utils/localStorage'
 
 const CART_KEY = 'cartItems'
 
+type CartItem = {
+  id: string
+  img: string
+  title: string
+  price: string
+}
+
 export type CartContextData = {
-  items: string[]
+  items: CartItem[]
 }
 
 export type CartProviderProps = {
@@ -28,8 +37,22 @@ export function CartProvider({ children }: CartProviderProps) {
     }
   }, [])
 
+  const { data } = useQueryGames({
+    skip: !cartItems.length,
+    variables: { where: { id: cartItems } }
+  })
+
   return (
-    <CartContext.Provider value={{ items: cartItems }}>
+    <CartContext.Provider
+      value={{
+        items: data?.games.map((game) => ({
+          id: game.id,
+          img: `http://localhost:1337${game.cover?.url}`,
+          price: formatPrice(game.price),
+          title: game.name
+        }))
+      }}
+    >
       {children}
     </CartContext.Provider>
   )

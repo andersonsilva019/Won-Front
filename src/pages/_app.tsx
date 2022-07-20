e88f0d8d35d535dd929efab6d5bf2a782e4ca60d
Anderson Silva <andersonnsilva@alu.ufc.br>
2022-07-20 11:28:59 -0300

    Adicionando o AuthProvider do NextAuth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { GlobalStyles } from 'styles/GlobalStyles'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
+import { Provider as NextAuthProvider } from 'next-auth/client'
 import { CartProvider } from 'hooks/use-cart'
 import { ThemeProvider } from 'styled-components'
 import theme from 'styles/theme'
@@ -12,22 +13,24 @@ export default function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <ThemeProvider theme={theme}>
-      <ApolloProvider client={client}>
-        <CartProvider>
-          <Head>
-            <title>Won Games</title>
-            <link rel="shortcut icon" href="/images/icon-512.png" />
-            <link rel="apple-touch-icon" href="/images/icon-512.png" />
-            <link rel="manifest" href="/manifest.json" />
-            <meta
-              name="description"
-              content="The best games stores in the world!"
-            />
-          </Head>
-          <GlobalStyles />
-          <Component {...pageProps} />
-        </CartProvider>
-      </ApolloProvider>
+      <NextAuthProvider session={pageProps.session}>
+        <ApolloProvider client={client}>
+          <CartProvider>
+            <Head>
+              <title>Won Games</title>
+              <link rel="shortcut icon" href="/images/icon-512.png" />
+              <link rel="apple-touch-icon" href="/images/icon-512.png" />
+              <link rel="manifest" href="/manifest.json" />
+              <meta
+                name="description"
+                content="The best games stores in the world!"
+              />
+            </Head>
+            <GlobalStyles />
+            <Component {...pageProps} />
+          </CartProvider>
+        </ApolloProvider>
+      </NextAuthProvider>
     </ThemeProvider>
   )
 }
